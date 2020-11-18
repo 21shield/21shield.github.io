d20d6636ebad1e4a347f704e998f7e1421b5f342
21shield <21netaly@gmail.com>
2020-11-18 14:07:58 -0500

    added scss packages

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -2,11 +2,11 @@ import React from "react"
 import Navbar from "../Containers/Navbar"
 import Footer from "../Containers/Footer"
 
-export default function Layout() {
+export default function Layout({ children }) {
   return (
     <div>
       <Navbar />
-
+      <main> {children} </main>
       <Footer />
     </div>
   )
