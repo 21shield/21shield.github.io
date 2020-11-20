0609dad73757db36832828ff1c87eb020699e776
21shield <21netaly@gmail.com>
2020-11-20 17:12:10 -0500

    fixed file locations

--- src/Containers/LandingPage.js
+++ src/Containers/LandingPage.js
@@ -1,12 +1,13 @@
 import React from "react"
+import "../styles/styles.scss"
 
 export default function LandingPage() {
   return (
     <>
-      <h1>about</h1>
-      <h1>Contact</h1>
-      <h1>Projects</h1>
-      <h1>me</h1>
+      <h1 className="about">__About__</h1>
+      <h1 className="Contact">--Contact--</h1>
+      <h1 className="Projects">~~Projects~~</h1>
+      <h1 className="Me">|| Me ||</h1>
     </>
   )
 }
