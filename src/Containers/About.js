9d54e0cfab798817cf55538e4af9233ee24a08f6
21shield <21netaly@gmail.com>
2021-01-12 21:53:03 -0500

    Fixed Landing page display

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -6,6 +6,14 @@ export default function About() {
       <h1 className="about">__About__</h1>
 
       <h1>About Me</h1>
+      <img src="" alt="" />
+      <p>
+        Lorem non sunt mollit minim elit consequat enim enim ex incididunt. Consectetur nostrud culpa culpa adipisicing ut labore consectetur occaecat est ipsum Lorem nulla minim sit. Anim amet non excepteur laborum veniam nostrud laboris voluptate. Cupidatat minim non dolore reprehenderit ipsum consequat mollit commodo aute occaecat id pariatur. Sunt et consequat non esse excepteur et ipsum magna qui culpa laborum nisi aute. Aliqua sunt incididunt laboris qui consectetur exercitation duis aute ea incididunt. Eiusmod amet tempor excepteur officia.
+
+        Ex officia cupidatat est pariatur nisi. Occaecat enim dolor nisi mollit nisi elit. Aute eu est magna nulla dolor do ipsum culpa ex ullamco ipsum id minim. Quis qui aliqua anim aute magna. Qui incididunt quis occaecat velit velit labore dolore sit sint nisi officia. Do nulla enim irure est ullamco aliquip.
+
+        Laboris minim velit adipisicing sit sit commodo sit minim ut deserunt commodo. Magna velit enim eu irure labore duis ullamco mollit adipisicing laboris aute commodo qui laborum. Occaecat ipsum deserunt sunt voluptate nostrud dolore. Quis aute qui ex non exercitation elit elit enim officia minim. Nostrud irure laboris ipsum esse cupidatat Lorem reprehenderit enim exercitation irure fugiat pariatur elit.
+      </p>
     </div>
   )
 }
--- src/Containers/LandingPage.js
+++ src/Containers/LandingPage.js
@@ -1,15 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "../Components/Layout"
+import About from "./About"
+import Media from "../Components/Media"
 import "../styles/styles.scss"
 
 export default function LandingPage() {
   return (
     <>
+    <Layout>
       <div className="landing">
         <h1 className="Me">|| Me Code ||</h1>
       </div>
       {/* on click this should lead the rest of the info */}
       <Link to="#about"> More </Link>
+      <About/>
+      <Media/>
+    </Layout>
     </>
   )
 }
