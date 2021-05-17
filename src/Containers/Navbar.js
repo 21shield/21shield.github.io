e7f5396cbc8a8f528c0d04217651ae7ca72c0e70
Netaly <netaly.ramirez@gmail.com>
2021-05-17 18:19:17 -0400

    update icon

--- src/Containers/Navbar.js
+++ src/Containers/Navbar.js
@@ -1,12 +1,27 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql  } from "gatsby"
+import Img from "gatsby-image"
 
 export default function Navbar() {
+  const data = useStaticQuery(graphql`
+    query {
+      icon: file(relativePath: { eq: "icon.jpeg" }) {
+        childImageSharp {
+          fluid(maxHeight: 200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    
+    }
+  `)
   return (
     <nav>
       <div className="header">
         <div className="header__inner-header">
-          <div className="header__logo">NR</div>
+          <div className="header__logo">
+            <Img fluid={data.icon.childImageSharp.fluid} alt="N Icon"/>
+          </div>
 
           <div className="header__navigation">
             <Link aria-label="about arthur section"to="#about"> About</Link>
