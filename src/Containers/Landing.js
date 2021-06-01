0c12ef02c9e31bf54710eca8ac63481a132e2be4
Netaly <netaly.ramirez@gmail.com>
2021-06-01 19:18:45 -0400

    changed file structure for index

---
+++ src/Containers/Landing.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import { FiArrowDownRight } from "react-icons/fi"
+import "../styles/index.scss"
+import Img from "gatsby-image"
+import Media from "../Components/Media"
+
+const Landing = () => {
+    const data = useStaticQuery(graphql`
+    query {
+      mom: file(relativePath: { eq: "Mom.jpeg" }) {
+        childImageSharp {
+          fluid(maxHeight: 500) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+    return (
+            <div className="landing">
+          <div className="landing__container">
+            <h1 className="landing__text">Netaly Ramirez</h1>
+
+            <div className="landing__main-image">
+              <Img
+                fluid={data.mom.childImageSharp.fluid}
+                alt="Mother and child"
+              />
+            </div>
+          </div>
+          <div className="landing__moreInfo">
+            <Link to="#about" aria-label="link to more info">
+              <FiArrowDownRight /> 
+            </Link>
+          </div>
+          <Media />
+        </div>
+    );
+}
+
+export default Landing;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,18 @@
 import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
-import { FiArrowDownRight } from "react-icons/fi"
-import Img from "gatsby-image"
 import "../styles/index.scss"
-import Media from "../Components/Media"
+import Landing from "../Containers/Landing"
 import About from "../Containers/About"
 import Layout from "../Components/Layout"
 import Projects from "../Containers/Projects"
 
 export default function Index() {
-  const data = useStaticQuery(graphql`
-    query {
-      mom: file(relativePath: { eq: "Mom.jpeg" }) {
-        childImageSharp {
-          fluid(maxHeight: 500) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
+ 
   return (
     <>
       <Layout>
-        <div className="landing">
-          <div className="landing__container">
-            <h1 className="landing__text">Netaly Ramirez</h1>
-
-            <div className="landing__main-image">
-              <Img
-                fluid={data.mom.childImageSharp.fluid}
-                alt="Mother and child"
-              />
-            </div>
-          </div>
-          <div className="landing__moreInfo">
-            <Link to="#about" aria-label="link to more info">
-              <FiArrowDownRight /> 
-            </Link>
-          </div>
-          <Media />
-        </div>
-
+        <Landing />
         <About />
         <Projects />
-
       </Layout>
     </>
   )
