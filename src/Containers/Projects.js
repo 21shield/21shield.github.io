cf4ed3036dbe9b6ebe7259e208c3411b8de6a1a4
Netaly <netaly.ramirez@gmail.com>
2021-06-09 02:09:10 -0400

    updated for deployment

--- src/Containers/Projects.js
+++ src/Containers/Projects.js
@@ -3,7 +3,7 @@ import React from "react"
 const Projects = () => {
   return (
     <div id="projects" className="container">
-      <h1>List of projects to go here</h1>
+      {/* <h1>List of projects to go here</h1> */}
       
     </div>
   )
