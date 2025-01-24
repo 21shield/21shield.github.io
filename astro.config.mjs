03a7ce54186c90873c4fb6bb3f5e98af450a0731
Netaly <25016486+21shield@users.noreply.github.com>
2025-01-24 18:23:29 -0500

    updated config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://21shield.github.io",
+  site: "https://21shield.github.io/",
   base: "/21shield.github.io",
 });
